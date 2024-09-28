6b856b35c04b8db180a248250fce4111da7f77c4
Linus Wedding <linus.wedding@gmx.de>
2024-09-28 14:13:53 +0200

    Column Interface setup

---
+++ src/main/java/de/exxcellent/challenge/Column.java
@@ -0,0 +1,25 @@
+package de.exxcellent.challenge;
+
+public interface Column<T>{
+    /**
+     * get value stored at the given row
+     * @param row index of the value to be retrieved
+     * @return value at given index
+     * @throws Exception exception if no value at the given index or index exceeds column length
+     */
+    T getValue(int row) throws Exception;
+
+    /**
+     * overwrite existing value at the given row
+     * @param value to be entered
+     * @param row index of the value to be overwritten
+     * @throws Exception exception if index exceeds column length
+     */
+    void setValue(T value, int row) throws Exception;
+
+    /**
+     * add a new value into the column, appending it at the end
+     * @param value value to be entered into the column
+     */
+    void addValue(T value);
+}
