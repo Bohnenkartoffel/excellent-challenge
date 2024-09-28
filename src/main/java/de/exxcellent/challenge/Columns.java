c76027b70203a79860e0baf515c693f272edbdd3
Linus Wedding <linus.wedding@gmx.de>
2024-09-28 15:05:50 +0200

    Added missing javadocs to Columns.java

--- src/main/java/de/exxcellent/challenge/Columns.java
+++ src/main/java/de/exxcellent/challenge/Columns.java
@@ -9,13 +9,24 @@ import java.util.ArrayList;
 public abstract class Columns<T extends Column> {
     private ArrayList<T> columns;
 
+    /**
+     * Creates a new, empty list of columns
+     */
     Columns() {
         columns = new ArrayList<>();
     }
 
+    /**
+     * Adds a column to the list of columns, appending it at the end
+     * @param column column to add
+     */
     public void addColumn(T column) {
         columns.add(column);
     }
 
+    /**
+     * retrieves a column from the list of columns
+     * @return a column
+     */
     public abstract T getColumn();
 }
