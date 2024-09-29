c423cc7c0115c95dac27dcedc5afa48f107e9f7f
Linus Wedding <linus.wedding@gmx.de>
2024-09-29 14:13:14 +0200

    Adapted Challenge.java to redesigned data structure, implementing a generic findLeastSpread method to be used by different challenges

--- src/main/java/de/exxcellent/challenge/utils/Challenge.java
+++ src/main/java/de/exxcellent/challenge/utils/Challenge.java
@@ -55,6 +55,40 @@ public abstract class Challenge {
     }
 
     /**
+     * Finds the row where column A and B have the least spread. A and B values are parsed as float.
+     * The result will be the value of outputColumn in the row with the least spread, choosing the first row if multiple
+     * rows share the minimal spread.
+     * @param outputColumnIndex index of the column to be used as output
+     * @param columnIndexA index of column A to be used for the spread search
+     * @param columnIndexB index of column B to be used for the spread search
+     * @return value of outputColumn at the row with the least spread between float-converted column A and B values
+     */
+    protected String findLeastSpread(int outputColumnIndex, int columnIndexA, int columnIndexB){
+        String[] outputColumn = data[outputColumnIndex];
+        String[] columnA = data[columnIndexA];
+        String[] columnB = data[columnIndexB];
+        float currentMinimalSpread = Float.MAX_VALUE;
+        String result = "No result found";
+
+        for (int i = 0; i < outputColumn.length; i++){
+            float a = Float.parseFloat(columnA[i]);
+            float b = Float.parseFloat(columnB[i]);
+            float spread = calculateSpread(a, b);
+            if (spread < currentMinimalSpread){
+                currentMinimalSpread = spread;
+                result = outputColumn[i];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Runs the challenge and returns the result in String form
+     * @return the result of the challenge
+     */
+    public abstract String run();
+
+    /**
      * Calculates absolute distance between two values
      * @param valueA first value
      * @param valueB second value
