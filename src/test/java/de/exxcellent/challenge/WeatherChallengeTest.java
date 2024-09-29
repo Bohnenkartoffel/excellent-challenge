1970369263c45e7e7cf42ba3de6552dee9a69e79
Linus Wedding <linus.wedding@gmx.de>
2024-09-29 15:23:30 +0200

    Clean-Up: Removed unnecessary imports

--- src/test/java/de/exxcellent/challenge/WeatherChallengeTest.java
+++ src/test/java/de/exxcellent/challenge/WeatherChallengeTest.java
@@ -3,10 +3,6 @@ package de.exxcellent.challenge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WeatherChallengeTest {
