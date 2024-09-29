12b0cc2aa12b2cdae9167a9eef2c0acce18e1838
Linus Wedding <linus.wedding@gmx.de>
2024-09-29 13:57:08 +0200

    Redesigned Challenge class to store CSV data in String[][] structure instead of previous CategorizedColumns structure

--- src/main/java/de/exxcellent/challenge/utils/Challenge.java
+++ src/main/java/de/exxcellent/challenge/utils/Challenge.java
@@ -2,6 +2,7 @@ package de.exxcellent.challenge.utils;
 
 import de.exxcellent.challenge.CSVFileReader;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -9,42 +10,49 @@ import java.util.ArrayList;
  * Rump class for the weather and football challenges
  */
 public abstract class Challenge {
-    protected CategorizedColumns columns;
+    protected String[][] data;
     protected CSVFileReader reader;
 
     /**
-     * Creates a new Challenge with the given data
-     * @param filepath to the CSV data
+     * Creates a new challenge from the given filepath with the given number of CSV categories and rows
+     * Data from the CSV is extracted automatically
+     * @param filepath to the CSV file for this challenge
+     * @param numberOfCategories contained in the CSV
+     * @param numberOfRows contained in the CSV
      */
-    public Challenge(String filepath){
+    public Challenge(String filepath, int numberOfCategories, int numberOfRows){
+        data = new String[numberOfCategories][numberOfRows];
+
         try {
             reader = new CSVFileReader(filepath);
-            extractCSVToCategorizedColumns();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        }
+
+        try {
+            extractCSV();
+        } catch (IOException e) {
+            System.out.println("CSV extraction error: " + e.getMessage());
         }
     }
 
     /**
-     * extracts data from the CSV file to a CategorizedColumns structure
-     * @throws IOException if there is an error reading from the CSV
+     * extracts data from the CSV file to a String[][] structure
+     * @throws IOException if a read attempt fails
      */
-    protected void extractCSVToCategorizedColumns() throws Exception {
-        CategorizedColumns columns = new CategorizedColumns();
-        if (reader.hasNextLine()){
-            ArrayList<String> categories = reader.nextLine();
-            columns = extractColumnValues(categories);
+    protected void extractCSV() throws IOException {
+        int x, y;
+        x = 0;
+        while (reader.hasNextLine()){
+            ArrayList<String> line = reader.nextLine();
+            y = 0;
+            for (String column : line) {
+                data[x][y] = column;
+                y++;
+            }
+            x++;
         }
-        this.columns = columns;
     }
-
-    /**
-     * retrieves values for each category of the CSV.
-     * Assumes that the first line containing the categories has already been read
-     * @param categories under which the values are to be stored
-     * @return CategorizedColumns with the given categories and read values
-     */
-    protected abstract CategorizedColumns extractColumnValues(ArrayList<String> categories) throws Exception;
 
     /**
      * Calculates absolute distance between two values
